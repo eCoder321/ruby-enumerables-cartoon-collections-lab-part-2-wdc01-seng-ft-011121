6ee484a67ca569a0e5ebcc3fe00f796e8850a5a9
Fem Adisa <femiwonderful@gmail.com>
2020-11-27 00:48:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls.find {
     |i| 
-    i.in(valid_calls)
+    valid_calls.include? i 
   }
 end
